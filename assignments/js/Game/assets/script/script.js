ba16c7cca2c1520f8b439dc3a1ac70bcb9dc964f
wishalNipun <wishal1974@gmail.com>
2022-11-15 16:33:18 +0530

    contentUpdate

--- assignments/js/Game/assets/script/script.js
+++ assignments/js/Game/assets/script/script.js
@@ -1,10 +1,34 @@
-
-idleImgNumber =0
+var idleImgNumber =0;
+var idleImgAnimationId;
 function idleAnimation(){
     idleImgNumber =idleImgNumber+1;
     if(idleImgNumber==9){
-        idleImgNumber=1;
+        idleImgNumber=0;
     }
     $('#character').attr('src','assets/img/Idle__00'+idleImgNumber+'.png');
 }
-setInterval(idleAnimation,100);
+
+function idleAnimationStart(){
+    idleImgAnimationId=setInterval(idleAnimation,100);
+}
+idleAnimationStart();
+
+var runImgNumber =0
+var runAnimationId;
+function runAnimation(){
+    runImgNumber =runImgNumber+1;
+    if(runImgNumber==9){
+        runImgNumber=0;
+    }
+    $('#character').attr('src','assets/img/Run__00'+runImgNumber+'.png');
+}
+
+function runAnimationStart(){
+    runAnimationId = setInterval(runAnimation,100);
+    clearInterval(idleImgAnimationId);
+}
+$("body").on('keydown', function (event) {
+    if (event.key == "Enter") {
+       runAnimationStart();
+    }
+});
